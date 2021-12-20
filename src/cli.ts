7b2e14c40846e84421400a727ed2a13a85542b87
Catriona Watson <catriona.watson@bbc.co.uk>
2021-12-20 22:11:40 +0000

    Adds inquirer prompts to CLI
    This commit adds in user interaction.
    
    A number of books to choose from is requested,
    then a title is requested for each.

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import { Command } from 'commander';
+import inquirer from 'inquirer';
 
 const cli = new Command();
 
@@ -7,7 +8,58 @@ cli
 
 cli.parse(process.argv);
 
+const requestBookChoices = () => {
+  console.log('Hello! Welcome to Book Selector.');
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "numberOfBooks",
+        message: "Number of books to choose from > ",
+        filter(userInput) {
+          return userInput.toLowerCase();
+        },
+        validate(userInput) {
+          // todo check not an float
+          if (parseInt(userInput)) {
+            return true;
+          } else {
+            return "Must be a number";
+          }
+        }
+      },
+    ])
+    .then(async (userInput) => {
+      const numberOfBooks: number = parseInt(userInput.numberOfBooks);
+      requestBookTitles(numberOfBooks);
+    });
+}
+
+const requestBookTitles = (numberOfBooks: number) => {
+  console.log(numberOfBooks);
+
+  let promptsArray: object[] = [];
+  for (let i = 1; i <= numberOfBooks; i++) {
+    promptsArray.push(
+      {
+        type: "input",
+        name: `bookTitle${i}`,
+        message: `Title of book ${i} >`,
+        filter(userInput: string) {
+          return userInput.toLowerCase();
+        }
+      });
+  }
+
+  inquirer
+    .prompt(promptsArray)
+    .then(async (parameters) => {
+      console.log(parameters);
+    });
+}
+
 const options = cli.opts();
 
-if ( Object.keys(options).length === 0 ) console.log("No options provided");
-if ( options.test ) console.log("Test");
+if (Object.keys(options).length === 0) requestBookChoices();
+if (options.test) console.log("Test");
