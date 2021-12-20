5cfe55bed69891e43b9cb28bb2cfabc5214067e2
Catriona Watson <catriona.watson@bbc.co.uk>
2021-12-20 12:14:20 +0000

    Adds CLI `-t` command as initial test
    Here I'm using Commander for the CLI.
    
    You can pass in options and execute methods
    depending on the options passed in.

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import { Command } from 'commander';
+
+const cli = new Command();
+
+cli
+  .option("-t, --test", "Test");
+
+cli.parse(process.argv);
+
+const options = cli.opts();
+if (options.test) console.log("Test");
